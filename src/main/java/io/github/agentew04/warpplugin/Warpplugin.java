e7e7eb7795e1dfac6725862dc78d67aac7ff9460
Agentew04 <digoappelt@gmail.com>
2021-10-08 21:49:31 -0300

    Add Files

---
+++ src/main/java/io/github/agentew04/warpplugin/Warpplugin.java
@@ -0,0 +1,17 @@
+package io.github.agentew04.warpplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Warpplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
