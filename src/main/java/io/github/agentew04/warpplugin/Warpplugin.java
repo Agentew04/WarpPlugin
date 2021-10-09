e82a545c68e41675e600c1eec93b074bf055de45
Agentew04 <digoappelt@gmail.com>
2021-10-08 23:22:33 -0300

    add some base code

--- src/main/java/io/github/agentew04/warpplugin/Warpplugin.java
+++ src/main/java/io/github/agentew04/warpplugin/Warpplugin.java
@@ -1,17 +1,44 @@
 package io.github.agentew04.warpplugin;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Warpplugin extends JavaPlugin {
 
+    public FileConfiguration config;
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        //commands
+        this.getCommand("warp").setExecutor(new WarpCommand(this));
+
+        //default values
+        config = this.getConfig();
+        config.addDefault("warps.zero.x",0);
+        config.addDefault("warps.zero.y",62);
+        config.addDefault("warps.zero.z",0);
+        this.saveDefaultConfig();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public void setCoords(String name, Coordinates coord){
+        config.set("warps."+name+"x",coord.x);
+        config.set("warps."+name+"y",coord.y);
+        config.set("warps."+name+"z",coord.z);
+    }
+    public Coordinates getCoords(String name){
+        if(config.isSet("warps."+name+"x")){
+            int x = config.getInt("warps."+name+"x");
+            int y = config.getInt("warps."+name+"y");
+            int z = config.getInt("warps."+name+"z");
+            return new Coordinates(x,y,z);
+        }else{
+            return Coordinates.zero;
+        }
+    }
 }
+
