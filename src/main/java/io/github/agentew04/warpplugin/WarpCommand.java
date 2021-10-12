750a3b1199e3137074fced44d90f5d75e5031e55
Agentew04 <digoappelt@gmail.com>
2021-10-12 03:02:01 -0300

    recreated warp command

--- src/main/java/io/github/agentew04/warpplugin/WarpCommand.java
+++ src/main/java/io/github/agentew04/warpplugin/WarpCommand.java
@@ -17,14 +17,23 @@ public class WarpCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player){
-            if(args!=null && args.length>=1){
+            if(args!=null && args.length==1){
                 Player player = (Player) sender;
-                Coordinates c = main.getCoords(args[0]);
-                player.teleport(new Location(player.getWorld(),c.x,c.y,c.z));
-                player.sendMessage(ChatColor.GREEN+"Teleportado com sucesso!");
+                Location loc = main.getWarp(args[0]);
+                if(loc==null){
+                    sender.sendMessage(ChatColor.RED+"Esse warp não existe!");
+                    return true;
+                }
+                player.teleport(loc);
+                player.sendMessage(ChatColor.GREEN+"Você foi teleportado com sucesso para: "+ChatColor.YELLOW+args[0]);
                 return true;
+            }else{
+                sender.sendMessage(ChatColor.RED+"Poucos ou argumentos demais!");
+                return false;
             }
+        }else{
+            sender.sendMessage(ChatColor.RED+"Apenas players ingame podem usar esse comando!");
+            return true;
         }
-        return false;
     }
 }
--- src/main/java/io/github/agentew04/warpplugin/Warpplugin.java
+++ src/main/java/io/github/agentew04/warpplugin/Warpplugin.java
@@ -1,5 +1,8 @@
 package io.github.agentew04.warpplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
